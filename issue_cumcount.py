b63cc11449c198ce87009f4a2b8290a4a73719bd
John Crousse <jcrousse@gmail.com>
2021-09-26 16:02:40 +0200

    removed some unnecessary prints

--- issue_cumcount.py
+++ issue_cumcount.py
@@ -1,15 +1,6 @@
 import cudf
 import pandas as pd
 
-from rmm._cuda.gpu import runtimeGetVersion, driverGetVersion
-
-# RecSys challenge 2015 dataset: https://www.kaggle.com/chadgostopp/recsys-challenge-2015
-
-print("cudf version: ", cudf.__version__)
-print("cuda runtime version: ", runtimeGetVersion())
-print("drivers version: ", driverGetVersion())
-
-
 DF_20_ROWS = 'archive/yoochoose-data/20_rows.dat'
 DF_424_ROWS = 'archive/yoochoose-data/424_rows.dat'
 
@@ -21,5 +12,5 @@ rapids_424 = cudf.read_csv(DF_424_ROWS, names=['session_id', 'timestamp', 'item_
 
 
 for df in [pandas_20, rapids_20, pandas_424, rapids_424]:
-    df['item_pos'] = df.groupby('session_id').cumcount()
+    df['cumcount'] = df.groupby('session_id').cumcount()
     print(df.loc[df['session_id'] == 121])
