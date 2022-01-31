bcbd32ef29a41163b4c89596e71f803f2855ea89
John Crousse <jcrousse@gmail.com>
2022-01-31 18:41:26 +0100

    first working version

--- WIP/raw_data_to_word_matrix.py
+++ WIP/raw_data_to_word_matrix.py
@@ -7,15 +7,17 @@ given input raw data as two columns: session_id and item_id;
 
 import cudf
 import cupy as cp
+import numpy as np
 from cupyx.time import repeat
 import random
 
 data_path = "data/train_set.dat"
 col_names = ['session_id', 'timestamp', 'item_id', 'category']
-cp_data = cudf.read_csv(data_path,  names=col_names, nrows=10000)[['session_id', 'timestamp',  'item_id']]\
+cp_data = cudf.read_csv(data_path,  names=col_names)[['session_id', 'timestamp',  'item_id']]\
     .sort_values(by='timestamp', ascending=False)\
     .drop_duplicates(subset=['session_id', 'item_id'], keep='first')
 
+# also works with strings
 cp_data["item_id"] = "item_" + cp_data["item_id"].astype(str)
 
 start_end_idx_df = cp_data\
@@ -24,62 +26,48 @@ start_end_idx_df = cp_data\
     .reset_index().rename(columns={"index": "end_idx"}) \
     .reset_index().rename(columns={"index": "start_idx"})
 
-sessions_per_item_cp = start_end_idx_df['session_id'].values
+session_array = start_end_idx_df['session_id'].values
 
 item_to_idx_df = start_end_idx_df.\
     groupby('item_id').\
     agg({"start_idx": "min", "end_idx": "max"})\
     .sort_index()
 
-item_to_idx_cp = item_to_idx_df.values
+item_to_idx_df["len"] = item_to_idx_df["end_idx"] - item_to_idx_df["start_idx"]
+
+item_to_idx_cp = cp.asnumpy(item_to_idx_df.values)
 
 itemid_to_itemidx = item_to_idx_df\
     .reset_index().rename(columns={"index": "item_id"})\
     .reset_index().set_index("item_id")['index']\
     .to_pandas().to_dict()
 
-sessions = cp_data['session_id'].unique()
+n_rand_sessions = 1000
+unique_sessions = cp_data['session_id'].unique()
+random_session_ids = random.choices(unique_sessions, k=n_rand_sessions)
+sessions_sample = cp_data[cp_data['session_id'].isin(random_session_ids)]\
+    .groupby('session_id').agg({'item_id': 'collect'}).to_pandas()['item_id'].values
 
-item_to_itemidx = cp_data[['item_id']] \
-                    .reset_index() \
-                    .set_index('item_id') \
-                    .to_pandas()['item_id'].to_dict()
 
 def random_session():
-    random_session_id = random.choice(sessions)
-    return cp_data[cp_data['session_id'] == random_session_id]['item_id'].values
-
-
-def cudf_indexing():
-    random_session_items = random_session()
-
-    # still need to reshape, but already too slow....
-    # For each item, array contains start/end indices of item (2 rows) in raw dataset
-    # return session values at all indices
-    return random_session_items
+    """maybe do this with pandas stuff? doesn't matter if on GPU or not, we assume we
+    start with a list of items"""
+    return random.choice(sessions_sample)
 
 
 def cupy_indexing():
     random_session_items = random_session()
-    sessions_scope = session_cp[(item_cp in random_session_items).any()]
-    # no easy way to get index of items common to both array
-    return sessions_scope
-
-
-
-# def custom_kernel_indexing():
-#     random_session_items = random_session()
-#     found_indices = cp.zeros()
-#     return index_match(
-#         (n_blocks_x, n_blocks_y),
-#         (64, 16),
-#         (item_cp, random_session_items, found_indices)
-#     )
-
-
+    items_idx = [itemid_to_itemidx[e] for e in random_session_items]
+    session_location = cp.asnumpy(item_to_idx_cp[items_idx])
+    max_len = session_location[:, 2].max()
+    sessions = cp.vstack([
+        cp.pad(session_array[np.arange(l[0], l[1] + 1)], (0, max_len - l[2]))
+        for l in session_location
+    ])
+    return sessions
 
 
 if __name__ == '__main__':
-    print(repeat(random_session, n_repeat=10))
+    print(repeat(random_session, n_repeat=100))
     # print(repeat(cudf_indexing,  n_repeat=10))
-    print(repeat(cupy_indexing, n_repeat=10))
+    print(repeat(cupy_indexing, n_repeat=100))
