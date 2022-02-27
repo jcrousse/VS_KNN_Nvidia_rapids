bff07c48baf9a12f3676070f17be9e13ba571173
John Crousse <jcrousse@gmail.com>
2022-02-27 17:18:06 +0100

    re-org demo to save/load

--- demo.py
+++ demo.py
@@ -1,55 +1,67 @@
-import cudf
-import os
-import cupy as cp
-from vs_knn.vs_knn import CupyVsKnnModel
 import gc
 import time
+import os
+import argparse
+import cudf
+import cupy as cp
+from vs_knn.vs_knn import CupyVsKnnModel
 from tqdm import tqdm
+import pickle
 
 
-dataset_filepath = 'archive/yoochoose-clicks.dat'
+def get_arguments():
+    """Get this script's command line arguments"""
+    parser = argparse.ArgumentParser(description='cuDF implementation of VS-KNN')
+    parser.add_argument('--train', '-t', dest='train', action='store_true', help="train the model")
+    parser.set_defaults(train=False)
+    args = parser.parse_args()
+    return args.train
 
 
-yoochoose_data = cudf.read_csv(dataset_filepath,
-                               usecols=[0, 1, 2],
-                               dtype={
-                                 'session_id': cp.dtype('int32'),
-                                 'item_id': cp.dtype('int32'),
-                                 'timestamp': cp.dtype('O')
-                                 },
-                               names=['session_id', 'timestamp', 'item_id'])
+def train():
+    dataset_filepath = 'archive/yoochoose-clicks.dat'
 
+    yoochoose_data = cudf.read_csv(dataset_filepath,
+                                   usecols=[0, 1, 2],
+                                   dtype={
+                                     'session_id': cp.dtype('int32'),
+                                     'item_id': cp.dtype('int32'),
+                                     'timestamp': cp.dtype('O')
+                                     },
+                                   names=['session_id', 'timestamp', 'item_id'])
 
-n_rows = yoochoose_data.shape[0]
-n_sessions = len(yoochoose_data['session_id'].unique())
-n_items = len(yoochoose_data['item_id'].unique())
-filesize = os.path.getsize(dataset_filepath)
+    n_rows = yoochoose_data.shape[0]
+    n_sessions = len(yoochoose_data['session_id'].unique())
+    n_items = len(yoochoose_data['item_id'].unique())
+    filesize = os.path.getsize(dataset_filepath)
 
-print(f"the dataset contains {round(n_rows / 10 ** 6)}M rows, ",
-      f"with {round(n_sessions / 10 ** 6)}M ",
-      f"sessions and {round(n_items / 10 ** 3)}K items",
-      f"\nOriginal file size: {round(filesize / 10 ** 6)}Mb")
+    print(f"the dataset contains {round(n_rows / 10 ** 6)}M rows, ",
+          f"with {round(n_sessions / 10 ** 6)}M ",
+          f"sessions and {round(n_items / 10 ** 3)}K items",
+          f"\nOriginal file size: {round(filesize / 10 ** 6)}Mb")
 
+    yoochoose_data['day'] = yoochoose_data['timestamp'].str.slice(start=0, stop=10)
 
-yoochoose_data['day'] = yoochoose_data['timestamp'].str.slice(start=0, stop=10)
+    all_days = yoochoose_data['day'].unique()
+    train_days = all_days[0:180]
+    print(all_days)
 
-all_days = yoochoose_data['day'].unique()
-train_days = all_days[0:180]
-print(all_days)
+    train_df = yoochoose_data[yoochoose_data['day'].isin(train_days)][['session_id', 'timestamp', 'item_id']]
+    test_df = yoochoose_data[~yoochoose_data['day'].isin(train_days)][['session_id', 'timestamp', 'item_id']]
 
-train_df = yoochoose_data[yoochoose_data['day'].isin(train_days)][['session_id', 'timestamp', 'item_id']]
-test_df = yoochoose_data[~yoochoose_data['day'].isin(train_days)][['session_id', 'timestamp', 'item_id']]
+    del yoochoose_data
+    gc.collect()
 
-del yoochoose_data
-gc.collect()
+    trained_model = CupyVsKnnModel(top_k=100, max_sessions_per_items=5000, max_item_per_session=10)
 
+    start = time.time()
+    trained_model.train(train_df)
+    end = time.time()
+    print(f"trained the model in {end - start} seconds")
 
-model = CupyVsKnnModel(top_k=100, max_sessions_per_items=5000, max_item_per_session=10)
+    test_sessions_array = get_test_examples(test_df)
 
-start = time.time()
-model.train(train_df)
-end = time.time()
-print(f"trained the model in {end - start} seconds")
+    return trained_model, test_sessions_array
 
 
 def get_test_examples(test_set):
@@ -60,9 +72,6 @@ def get_test_examples(test_set):
         .to_pandas()\
         .values
     return test_array
-
-
-test_sessions_array = get_test_examples(test_df)
 
 
 def session_to_xy(items_in_session):
@@ -95,4 +104,19 @@ def test_a_model(model, test_data):
     return time_per_iter, hr20
 
 
-itertime_rd, hr_rd = test_a_model(model, test_sessions_array)
+if __name__ == '__main__':
+    train_flag = get_arguments()
+
+    if train_flag:
+        model, test_array = train()
+        model.save('saved_model')
+        with open('test_data.pkl', 'wb') as f:
+            pickle.dump(test_array, f)
+
+    else:
+        model = CupyVsKnnModel()
+        model.load('saved_model')
+        with open('test_data.pkl', 'rb') as f:
+            test_array = pickle.load(f)
+
+    itertime_rd, hr_rd = test_a_model(model, test_array)
